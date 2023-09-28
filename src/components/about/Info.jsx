84cab20011318b3bfc0e37ce77a8ff0570a65e29
Alvaro Barba <abarba.lod@gmail.com>
2023-09-28 11:47:06 +0200

    project number

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -14,7 +14,7 @@ const Info = () => {
             <div className="about__box">
                 <i className="bx bx-briefcase-alt about__icon"></i>
                 <h3 className="about__title">Proyectos</h3>
-                <span className="about__subtitle">5 Proyectos</span>
+                <span className="about__subtitle">10 Proyectos</span>
             </div>
 
             <div className="about__box">
