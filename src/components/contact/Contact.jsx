9ad3d681a40371d8ccb9ad79f7d72dcae178f642
Alvaro Barba <abarba.lod@gmail.com>
2023-10-10 18:15:01 +0200

    contact update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
 
     return (
         <section className="contact section" id="contact">
-            <h2 className="section__title">Ponerse en contacto</h2>
+            <h2 className="section__title">Contacto</h2>
             <span className="section__subtitle">Contacta conmigo</span>
             <div className="contact__container container grid">
 
